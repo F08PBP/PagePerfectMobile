3b0236d9c186d51610db8d9b5cf0e481f4bc28d3
HanifRifky <114400903+HanifRifky@users.noreply.github.com>
2023-12-20 16:30:59 +0700

    Menyesuaikan link website django

--- lib/screens/umum/login.dart
+++ lib/screens/umum/login.dart
@@ -1,13 +1,11 @@
-// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:pageperfectmobile/modules/employee/screens/mainEmployee.dart';
 import 'package:pageperfectmobile/modules/member/screens/BookPage.dart';
 import 'package:pageperfectmobile/modules/member/screens/mainMember.dart';
+import 'package:pageperfectmobile/screens/temporary/menu.dart';
 import 'package:pageperfectmobile/screens/umum/user.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:pageperfectmobile/modules/writer/screens/writer_page.dart';
 
 class LoginApp extends StatelessWidget {
   const LoginApp({super.key});
@@ -42,14 +40,14 @@ class _LoginPageState extends State<LoginPage> {
       case 'Member':
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => const HomeMemberPage()),
+          MaterialPageRoute(builder: (context) => HomeMemberPage()),
         );
         break;
       case 'Writer':
         //Sesuaiin aja sama modul masing2
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => HomeWriterPage()),
+          MaterialPageRoute(builder: (context) => MyHomePage()),
         );
         break;
       case 'Employee':
@@ -107,8 +105,6 @@ class _LoginPageState extends State<LoginPage> {
 
                   if (role == "Member") {
                     money = response['money'] ?? 0;
-                  } else if (role == "Writer") {
-                    loggedUsername = uname;
                   }
 
                   loggedInUser =
